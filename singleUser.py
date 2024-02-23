372f244b4c0a24ffd7abd4c81500a7f4707c90a0
Mason <123887095+cyclothymia@users.noreply.github.com>
2024-02-23 11:00:44 +1100

    Update singleUser.py
    Added universal operating system support.

--- singleUser.py
+++ singleUser.py
@@ -3,7 +3,7 @@ from modules.tables import Table
 from modules.userscrape import singleUser
 
 if __name__ == "__main__":
-    jsonpath = "data/json/users/"
+    jsonpath = os.path.join("data", "json", "groups")
     if not os.path.exists(jsonpath):
         os.makedirs(jsonpath)
 
